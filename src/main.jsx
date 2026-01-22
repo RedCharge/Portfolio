84a64ccf1ea9f42940ea43812a37e519908c08b6
Redcharger <kwameadjei6780@gmail.com>
2026-01-22 15:49:27 +0000

    Add project files

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './home.jsx' // Make sure this path is correct
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
